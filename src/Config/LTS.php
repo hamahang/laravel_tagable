0e7f8fa1cfb2b80deee1b71f1b4d5838535b814f
faramarz sadeghi <faramarz.sadeghi89@gmail.com>
2018-09-08 16:11:14 +0430

    gg

--- src/Config/LTS.php
+++ src/Config/LTS.php
@@ -8,10 +8,8 @@ return [
     // you can change default route from sms-admin to anything you want
     'backend_lts_route_prefix' => 'LTS',
     'frontend_lts_route_prefix' => 'LTS',
-    // SMS.ir Api Key
-    'api-key' => env('SMSIR-API-KEY','Your api key'),
     // ======================================================================
     //allow user to upload private file in filemanager
     'userModel'=>'App\User',
-    'multiLang'=>'faq_sampleLang'
+    'multiLang'=> env('LPM_MULTILANG', 'faq_sampleLang'),
 ];
