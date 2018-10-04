2c6503f88274e4bf09dfb3c1153365991ebafaea
adel <adel.raheli@gmail.com>
2018-10-05 00:37:52 +0330

    config change and fixed issue

--- src/Config/LTS.php
+++ src/Config/LTS.php
@@ -3,13 +3,13 @@
 return [
 
     /* Important Settings */
-    'backend_lts_middlewares'   => explode(',', env('BACKEND_lTS_MIDDLEWARES', 'web')),
-    'frontend_lts_middlewares'  => explode(',', env('FRONTEND_lTS_MIDDLEWARES', 'web')),
+    'backend_lts_middlewares'   => explode(',', env('LTS_BACKEND_MIDDLEWARES', 'web')),
+    'frontend_lts_middlewares'  => explode(',', env('LTS_FRONTEND_MIDDLEWARES', 'web')),
     // you can change default route from sms-admin to anything you want
-    'backend_lts_route_prefix'  => env('BACKEND_lTS_ROUTE_PERFIX', 'lTS'),
-    'frontend_lts_route_prefix' => env('FRONTEND_lTS_ROUTE_PERFIX', 'lTS'),
+    'backend_lts_route_prefix'  => env('LTS_BACKEND_ROUTE_PERFIX', 'lTS'),
+    'frontend_lts_route_prefix' => env('LTS_FRONTEND_ROUTE_PERFIX', 'lTS'),
     // ======================================================================
     //allow user to upload private file in filemanager
-    'user_model'                => env('lTS_USER_MODEL', 'App\User'),
-    'multi_lang'                => env('lTS_MULTI_LANG', 'LTS_SampleLang'),
+    'user_model'                => env('LTS_USER_MODEL', 'App\User'),
+    'multi_lang'                => env('LTS_MULTI_LANG', 'LTS_SampleLang'),
 ];
